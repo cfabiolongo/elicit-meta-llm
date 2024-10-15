79844f8bbe61c96c504cbb12e4d318db598783bd
fabio <fabiuslongo@gmail.com>
2024-10-15 17:50:02 +0200

    +validation code

--- validate_metrics.py
+++ validate_metrics.py
@@ -18,7 +18,7 @@ bertscore = load("bertscore")
 # rouge = load("rouge")
 # bleu = load("bleu")
 
-filename = 'gpt_preds_t0.1'
+filename = 'gpt_preds_t0.6'
 
 df = pd.read_excel(f'dataset/{filename}.xlsx')
 dataset = Dataset.from_pandas(df)
@@ -28,6 +28,11 @@ print("#dataset items: ", len(dataset))
 rouge_precisions = []
 rouge_recalls = []
 rouge_f1s = []
+
+bert_precisions = []
+bert_recalls = []
+bert_f1s = []
+
 matches = []
 
 # bleu_scores = []
@@ -35,31 +40,27 @@ matches = []
 match = 0
 rouge = Rouge()
 
-# Calcola BERTScore in modo aggregato
-results_bertscore = bertscore.compute(predictions=dataset['Generated_Response'], references=dataset['response'], model_type="microsoft/deberta-v2-xxlarge-mnli")
-
-precision_scores = results_bertscore['precision']
-recall_scores = results_bertscore['recall']
-f1_scores = results_bertscore['f1']
-
-precision_media = statistics.mean(precision_scores)
-recall_media = statistics.mean(recall_scores)
-f1_media = statistics.mean(f1_scores)
-
-print("\nMedia della precisione (BERTScore):", precision_media)
-print("Media della recall (BERTScore):", recall_media)
-print("Media della f1 (BERTScore):", f1_media)
-
 for i in range(len(dataset)):
 
     print(f"\n---------------- Record #{i}:\n")
     
     d = dataset[i]
 
+    results_bert = bertscore.compute(predictions=[d['Generated_Response']], references=[d['response']], model_type="microsoft/deberta-v2-xxlarge-mnli")
+
+    # BERTSCORE section
+    precision_scores = results_bert['precision']
+    recall_scores = results_bert['recall']
+    f1_scores = results_bert['f1']
+
+    bert_precisions.append(precision_scores)
+    bert_recalls.append(recall_scores)
+    bert_f1s.append(f1_scores)
+
+    # ROUGE section
     rouge_score = rouge.get_scores(d['response'], d['Generated_Response'])[0]['rouge-l']
     print("\nrouge-l: ", rouge_score)
 
-    # Aggiungi i punteggi ROUGE alle liste
     rouge_precisions.append(rouge_score['p'])
     rouge_recalls.append(rouge_score['r'])
     rouge_f1s.append(rouge_score['f'])
@@ -71,16 +72,32 @@ for i in range(len(dataset)):
     else:
         matches.append("???")
 
+
     # Calcola BLEU per questa predizione
     # bleu_result = bleu.compute(predictions=[gen], references=[[d['response']]])
     # bleu_scores.append(bleu_result['bleu'])
 
+# Appiattisci le liste di precision, recall e f1
+flat_bert_precisions = [item for sublist in bert_precisions for item in sublist]
+flat_bert_recalls = [item for sublist in bert_recalls for item in sublist]
+flat_bert_f1s = [item for sublist in bert_f1s for item in sublist]
+
+# Calcola la media
+precision_media = statistics.mean(flat_bert_precisions)
+recall_media = statistics.mean(flat_bert_recalls)
+f1_media = statistics.mean(flat_bert_f1s)
+
+print("\nMedia della precisione (BERTScore):", precision_media)
+print("Media della recall (BERTScore):", recall_media)
+print("Media della f1 (BERTScore):", f1_media)
+
+
 # writing excel dataframe with predictions (Generated_Response)
 file_output = f"{filename}_validated"
 
-df['Precision'] = precision_scores
-df['Recall'] = recall_scores
-df['f1'] = f1_scores
+df['Precision'] = flat_bert_precisions
+df['Recall'] = flat_bert_recalls
+df['f1'] = flat_bert_f1s
 
 df['RG_L_P'] = rouge_precisions  # Aggiungi ROUGE precision per ogni record
 df['RG_L_R'] = rouge_recalls        # Aggiungi ROUGE recall per ogni record
